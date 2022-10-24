e782ffb755ff50224901dffc418692a1aaed4138
miniice <miniice@iice.fun>
2022-10-24 14:17:45 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,15 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  margin: 0;
 }
-
+#app {
+  width: 100vw;
+  height: 100vh;
+}
 nav {
+  position: absolute;
   padding: 30px;
 
   a {
